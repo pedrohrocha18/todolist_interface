8a8bbcaaa2524af6a789a21908db094589703569
Pedro H. Rocha <phr.tech@outlook.com>
2024-12-13 14:12:12 -0300

    teste user login

--- src/routes/register/Register.jsx
+++ src/routes/register/Register.jsx
@@ -45,43 +45,41 @@ const Register = () => {
         setTimeout(() => {
           navigate("/login");
         }, 2500);
+      }
 
-        if (response.error) {
-          throw { error: response.status };
-        }
+      if (response.error) {
+        throw { error: response.status };
       }
     } catch (error) {
       const errorCode = error["error"];
 
-      console.log(errorCode)
+      switch (errorCode) {
+        case 400:
+          toast.error(
+            "Todos os campos são obrigatórios. Verifique os dados e tente novamente."
+          );
+          break;
+        case 406:
+          toast.error(
+            "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais."
+          );
+          break;
+        case 409:
+          toast.error("O e-mail já está em uso.");
+          break;
+        case 422:
+          toast.error("E-mail inválido.");
+          break;
 
-      // switch (errorCode) {
-      //   case 400:
-      //     toast.error(
-      //       "Todos os campos são obrigatórios. Verifique os dados e tente novamente."
-      //     );
-      //     break;
-      //   case 406:
-      //     toast.error(
-      //       "A senha deve conter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais."
-      //     );
-      //     break;
-      //   case 409:
-      //     toast.error("O e-mail já está em uso.");
-      //     break;
-      //   case 422:
-      //     toast.error("E-mail inválido.");
-      //     break;
-
-      //   case 500:
-      //     toast.error(
-      //       "Erro no servidor. Por favor, tente novamente mais tarde."
-      //     );
-      //     break;
-      //   default:
-      //     toast.error("Ocorreu um erro desconhecido. Tente novamente.");
-      //     break;
-      // }
+        case 500:
+          toast.error(
+            "Erro no servidor. Por favor, tente novamente mais tarde."
+          );
+          break;
+        default:
+          toast.error("Ocorreu um erro desconhecido. Tente novamente.");
+          break;
+      }
     }
   };
 
